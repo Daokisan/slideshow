5e447b5e88721cc488a641f78bee105ce34902ba
daokisan <daokisan@hotmail.com>
2022-06-11 20:24:18 -0300

    Atualizaçao imagens e fundo

--- slideShow.js
+++ slideShow.js
@@ -1,12 +1,12 @@
 'use strict';
 
 const images = [
-    { 'id': '1', 'url':'./img/chrono.jpg' },
-    { 'id': '2', 'url':'./img/inuyasha.jpg' },
-    { 'id': '3', 'url':'./img/darling.jpg' },
-    { 'id': '4', 'url':'./img/dororo.jpg' },
-    { 'id': '5', 'url':'./img/yuyuhakusho.jpg' },
-    { 'id': '6', 'url':'./img/akt.jpg' },
+    { 'id': '1', 'url':'./img/imagem1.jpg' },
+    { 'id': '2', 'url':'./img/imagem2.jpg' },
+    { 'id': '3', 'url':'./img/imagem3.jpg' },
+    { 'id': '4', 'url':'./img/imagem4.jpg' },
+    { 'id': '5', 'url':'./img/imagem5.jpg' },
+    { 'id': '6', 'url':'./img/imagem6.jpg' },
 ]
 
 const containerItems = document.querySelector('#container-items');
